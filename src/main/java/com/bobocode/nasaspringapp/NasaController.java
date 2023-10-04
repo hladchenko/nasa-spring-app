ffdf11bff0bd643ce3eeee14160cf28de37039bb
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-05 01:11:40 +0300

    Rfactor to stream

--- src/main/java/com/bobocode/nasaspringapp/NasaController.java
+++ src/main/java/com/bobocode/nasaspringapp/NasaController.java
@@ -2,18 +2,17 @@ package com.bobocode.nasaspringapp;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 @Controller("/")
@@ -22,57 +21,28 @@ public class NasaController {
     @Value("${key}")
     private String key;
 
-
-    @Cacheable("picture")
+    @Cacheable("largest-picture")
     @GetMapping
-
-    public ResponseEntity<byte[]> get(@RequestParam int sol) throws JsonProcessingException {
-
-        long max = 0;
-        byte[] item;
-        URI itemSrc = null;
-
+    public ResponseEntity<?> get(@RequestParam int sol) throws JsonProcessingException {
         RestTemplate template = new RestTemplate();
-
-        ResponseEntity<String> response = template.getForEntity(String.format("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=%d&api_key=%s", sol, key), String.class);
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.getBody());
-        JsonNode photos = root.path("photos");
-        for (JsonNode node : photos) {
-
-            URI source = URI.create(node.path("img_src").textValue());
-            HttpHeaders headers = template.headForHeaders(source);
-
-            if (headers.getLocation() != null) {
-                source = headers.getLocation();
-                headers = template.headForHeaders(source);
-            }
-
-            long length = headers.getContentLength();
-
-            if (length > max) {
-                max = length;
-                itemSrc = source;
-            }
-        }
-
-        if (itemSrc != null) {
-            item = template.getForObject(itemSrc, byte[].class);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(item);
+        return Objects.requireNonNull(template.getForEntity(String.format("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=%d&api_key=%s", sol, key), JsonNode.class).getBody())
+                .get("photos")
+                .findValuesAsText("img_src")
+                .parallelStream()
+                .collect(Collectors.toMap(this::getRedirectedResponse, src -> src))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingLong(o -> o.getKey().getHeaders().getContentLength()))
+                .orElseThrow().getKey();
     }
 
-    @GetMapping("/largest")
-    public void getLargest(@RequestParam int sol) {
-        // TODO: Implement using HTTP Client
-    }
-
-    @GetMapping("/largestImage")
-    public void getLargestImage(@RequestParam int sol) {
-        // TODO: Implement using Web Client
+    private ResponseEntity<byte[]> getRedirectedResponse(String src) {
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<byte[]> entry = template.getForEntity(src, byte[].class);
+        while (entry.getStatusCode().is3xxRedirection()) {
+            entry = template.getForEntity(Objects.requireNonNull(entry.getHeaders().getLocation()), byte[].class);
+        }
+        return entry;
     }
 
 }
